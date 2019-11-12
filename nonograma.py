885619d9b2a6fceb907374a5700417d49e7924af
IsaiasRRendon <42423869+IsaiasRRendon@users.noreply.github.com>
2019-11-11 22:32:18 -0600

    interfaz grafica y metodo main

--- nonograma.py
+++ nonograma.py
@@ -103,4 +103,101 @@ class Nonograma:
             total_cuentas_col.append(cuenta_actual_col)
             cuenta_actual_col = []
 
+        # Nueva ventana
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        # Titulo de la ventana
+        self.window.set_title("Demo Nonograma")
+
+        # Delete event
+        self.window.connect("delete_event", self.delete_event)
+
+        # Borde de la ventana
+        self.window.set_border_width(0)
+
+        # Main layout
+        vbox = gtk.VBox(True, 0)
+        self.window.add(vbox)
+
+        # Determina el padding necesario para las columnas
+        max_col = 0
+        for i in range(10):
+            actual = len(total_cuentas_col[i])
+
+            if actual > max_col:
+                max_col = actual
+
+      
+        for i in range(10):
+            padding = max_col - len(total_cuentas_col[i])
+
+            for j in range(padding):
+                total_cuentas_col[i].insert(0, '')
+
+        # Determina el padding necesario para los renglones
+        max_ren = 0
+        for i in range(10):
+            actual = len(total_cuentas_ren[i])
+
+            if actual > max_ren:
+                max_ren = actual
+
         
+        for i in range(10):
+            padding = max_ren - len(total_cuentas_ren[i])
+
+            for j in range(padding):
+                total_cuentas_ren[i].insert(0, '')
+
+        
+        for i in range(max_col):
+            hbox = gtk.HBox(True, 0)
+            vbox.add(hbox)
+
+            
+            for j in range(max_ren):
+                label = gtk.Label('')
+                label.show()
+                hbox.add(label)
+
+            
+            for j in range(10):
+                label = gtk.Label(total_cuentas_col[j][i])
+                label.show()
+                hbox.add(label)
+
+            hbox.show()
+
+        # Area de juego
+        for i in range(10):
+            hbox = gtk.HBox(True, 0)
+            vbox.add(hbox)
+
+            # Renglones
+            for count in total_cuentas_ren[i]:
+                label = gtk.Label(count)
+                label.show()
+                hbox.add(label)
+
+            # Checkboxes
+            for j in range(10):
+                button = gtk.CheckButton()
+                button.connect('toggled', self.callback, grid[i][j], total_on)
+                hbox.pack_start(button, True, True, 2)
+                button.show()
+        
+            hbox.show()
+
+        vbox.show()        
+
+        self.window.show()
+
+def main():
+    gtk.main()
+    return 0       
+
+if __name__ == "__main__":
+
+    # nonograma 10x10
+    Nonograma([[random.randint(0, 1) for column in range(10)] for row in range(10)])
+    main()
